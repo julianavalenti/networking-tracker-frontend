9c4ffb535ae7037ceddb21c91347940a69611578
julianavalenti <julianabmartinez2@icloud.com>
2023-05-20 10:44:25 -0400

    fixes menu css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import Avatar from '../img/img.gif'
-import "../styles/components/sidebar.sass"
-
+import React from 'react';
+import Avatar from '../img/img.gif';
+import '../styles/components/sidebar.sass';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <aside id="sidebar">
-      
-
       <Link className="menu" to="/">
-        Home
+        HOME
       </Link>
       <Link className="menu" to="/network">
-        Network
+        NETWORKING TRACKER
       </Link>
       <Link className="menu" to="/events">
-        Events
+        EVENTS
       </Link>
     </aside>
   );
 };
 
-export default Sidebar
+export default Sidebar;
