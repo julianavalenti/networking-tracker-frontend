7299024d16fcdd56ca39edabc0bfb30004c499d6
julianavalenti <julianabmartinez2@icloud.com>
2023-05-22 21:54:23 -0400

    pushes

---
+++ src/components/Events.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import EventsIndex from "./pages/EventsIndex";
+import EventsShow from "./pages/EventsShow";
+import '../styles/components/mainpage.sass';
+
